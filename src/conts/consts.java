bd578873fd12e223dd7ceefc8f42c49eb0fa3f7e
josedomingos919 <josedomingos919@gmail.com>
2023-04-09 07:03:06 +0100

    feat: add expessial char

--- src/conts/consts.java
+++ src/conts/consts.java
@@ -3,9 +3,5 @@ package conts;
 public class consts {
     public static final String FILE_NAME = "input.txt";
 
-    public static final String REGEX_NUMBER = "0123456789";
-    public static final String REGEX_ABC_LOWER = "abcdefghijklmnopqrstuvwxyz";
-    public static final String REGEX_ABC_UPDER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    public static final String REGEX_ABC_MIX = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    public static final String REGEX_ID = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz_";
+    public static final String EXPESSIAL_CHAR = "nfr\\0";
 }
